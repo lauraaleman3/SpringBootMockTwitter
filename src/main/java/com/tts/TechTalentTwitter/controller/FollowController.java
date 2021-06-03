5f5839ea66a3126a10ca0dc4ec6e962a674adb34
Laura Aleman <79146743+lauraaleman3@users.noreply.github.com>
2021-06-03 08:47:30 -0500

    Finished project

--- src/main/java/com/tts/TechTalentTwitter/controller/FollowController.java
+++ src/main/java/com/tts/TechTalentTwitter/controller/FollowController.java
@@ -16,6 +16,7 @@ public class FollowController {
     @Autowired
     private UserService userService;
 
+
     @PostMapping("/follow/{username}")
     public String follow(@PathVariable("username") String username,
                          HttpServletRequest request) {
@@ -25,19 +26,21 @@ public class FollowController {
         followers.add(loggedInUser);
         userToFollow.setFollowers(followers);
         userService.save(userToFollow);
-        return "redirect: " + request.getHeader("Referer");
+//        return "redirect: " + request.getHeader("Referer");
+        return "redirect:" + request.getHeader("Referer");
+
     }
 
     @PostMapping("/unfollow/{username}")
     public String unfollow(@PathVariable("username") String username,
-                         HttpServletRequest request) {
+                           HttpServletRequest request) {
         User loggedInUser = userService.getLoggedInUser();
-        User userToUnFollow = userService.findByUserName(username);
-        List<User> followers = userToUnFollow.getFollowers();
+        User userToUnfollow = userService.findByUserName(username);
+        List<User> followers = userToUnfollow.getFollowers();
         followers.remove(loggedInUser);
-        userToUnFollow.setFollowers(followers);
-        userService.save(userToUnFollow);
-        return "redirect: " + request.getHeader("Referer");
+        userToUnfollow.setFollowers(followers);
+        userService.save(userToUnfollow);
+        return "redirect:" + request.getHeader("Referer");
     }
 
 }
--- src/main/java/com/tts/TechTalentTwitter/service/TweetService.java
+++ src/main/java/com/tts/TechTalentTwitter/service/TweetService.java
@@ -1,17 +1,19 @@
 package com.tts.TechTalentTwitter.service;
 
+
 import com.tts.TechTalentTwitter.model.Tag;
 import com.tts.TechTalentTwitter.model.Tweet;
+import com.tts.TechTalentTwitter.model.TweetDisplay;
 import com.tts.TechTalentTwitter.model.User;
 import com.tts.TechTalentTwitter.repository.TagRepository;
 import com.tts.TechTalentTwitter.repository.TweetRepository;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,23 +26,22 @@ public class TweetService {
     @Autowired
     private TagRepository tagRepository;
 
-    public List<Tweet> findAll() {
+    public List<TweetDisplay> findAll() {
         List<Tweet> tweets = tweetRepository.findAllByOrderByCreatedAtDesc();
         return formatTweets(tweets);
     }
 
-    public List<Tweet> findAllByUser(User user) {
+    public List<TweetDisplay> findAllByUser(User user) {
         List<Tweet> tweets = tweetRepository.findAllByUserOrderByCreatedAtDesc(user);
-        return tweets;
+        return formatTweets(tweets);
     }
 
-    public List<Tweet> findAllByUsers(List<User> users) {
-        List<Tweet> tweets = tweetRepository.findAllByUserInOrderByCreatedAtDesc(users);
-        return tweets;
+    public List<TweetDisplay> findAllByUsers(List<User> users) {
+        return formatTweets(tweetRepository.findAllByUserInOrderByCreatedAtDesc(users));
     }
 
-    public List<Tweet> findAllWithTag(String tag) {
-        List<Tweet> tweets = tweetRepository.findByTags_PhraseOrderByCreatedAtDesc(tag);
+    public List<TweetDisplay> findAllWithTag(String phrase) {
+        List<Tweet> tweets = tweetRepository.findByTags_PhraseOrderByCreatedAtDesc(phrase);
         return formatTweets(tweets);
     }
 
@@ -50,7 +51,7 @@ public class TweetService {
     }
 
     private void handleTags(Tweet tweet) {
-        List<Tag> tags = new ArrayList<Tag>();
+        List<Tag> tags = new ArrayList<>();
         Pattern pattern = Pattern.compile("#\\w+");
         Matcher matcher = pattern.matcher(tweet.getMessage());
         while (matcher.find()) {
@@ -66,10 +67,11 @@ public class TweetService {
         tweet.setTags(tags);
     }
 
-    private List<Tweet> formatTweets(List<Tweet> tweets) {
+    private List<TweetDisplay> formatTweets(List<Tweet> tweets) {
         addTagLinks(tweets);
         shortenLinks(tweets);
-        return tweets;
+        List<TweetDisplay> displayTweets = formatTimestamps(tweets);
+        return displayTweets;
     }
 
     private void addTagLinks(List<Tweet> tweets) {
@@ -81,9 +83,10 @@ public class TweetService {
             while (matcher.find()) {
                 tags.add(matcher.group());
             }
-            for (String tag : tags) {
+            for (String tag: tags) {
                 message = message.replaceAll(tag,
-                        "<a class=\"tag\" href=\"/tweets/" + tag.substring(1).toLowerCase() + "\">" + tag + "</a>");
+                        "<a class=\"tag\" href=\"/tweets/"
+                                + tag.substring(1).toLowerCase() + "\">" + tag + "</a>");
             }
             tweet.setMessage(message);
         }
@@ -99,13 +102,34 @@ public class TweetService {
                 String shortenedLink = link;
                 if (link.length() > 23) {
                     shortenedLink = link.substring(0, 20) + "...";
-                    message = message.replace(link,
-                            "<a class=\"tag\" href=\"" + link + "\" target=\"_blank\">" + shortenedLink + "</a>");
                 }
-                tweet.setMessage(message);
+                message = message.replace(link,
+                        "<a class=\"tag\" href=\"" + link + "\" target=\"_blank\">" + shortenedLink + "</a>");
             }
-
+            tweet.setMessage(message);
         }
+    }
+
+    private List<TweetDisplay> formatTimestamps(List<Tweet> tweets) {
+        List<TweetDisplay> response = new ArrayList<>();
+        PrettyTime prettyTime = new PrettyTime();
+        SimpleDateFormat simpleDate = new SimpleDateFormat("M/d/yy");
+        Date now = new Date();
+        for (Tweet tweet : tweets) {
+            TweetDisplay tweetDisplay = new TweetDisplay();
+            tweetDisplay.setUser(tweet.getUser());
+            tweetDisplay.setMessage(tweet.getMessage());
+            tweetDisplay.setTags(tweet.getTags());
+            long diffInMillies = Math.abs(now.getTime() - tweet.getCreatedAt().getTime());
+            long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+            if (diff > 3) {
+                tweetDisplay.setDate(simpleDate.format(tweet.getCreatedAt()));
+            } else {
+                tweetDisplay.setDate(prettyTime.format(tweet.getCreatedAt()));
+            }
+            response.add(tweetDisplay);
+        }
+        return response;
     }
 
 
